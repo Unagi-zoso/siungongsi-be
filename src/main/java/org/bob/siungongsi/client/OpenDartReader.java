c5cfb45a0d6b094f50249f3485675fc852ee9806
Unagi-zoso <Unagi.zoso@gmail.com>
2025-03-17 19:40:16 +0900

    feat: OpenDartReader 개발
    OpenDart API를 좀 더 비즈니스 로직에 부합하게 사용하는 객체
    OpenDart 에서는 한 번에 최대 100개씩 페이지로 목록 조회를 지원하는데
    while 문을 통해 한 번에 반환하게 만든다.
    작년 기준 하루에 들어오는 최대 공시 수는 약 5000개이다.

---
+++ src/main/java/org/bob/siungongsi/client/OpenDartReader.java
@@ -0,0 +1,71 @@
+package org.bob.siungongsi.client;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bob.siungongsi.client.clientinterface.OpenDartClientInterface;
+import org.bob.siungongsi.client.dto.OpenDartDtos.GongsiData;
+import org.bob.siungongsi.client.dto.OpenDartDtos.GongsiListResponse;
+import org.bob.siungongsi.service.RankedCompanyManager;
+import org.bob.siungongsi.service.TodayProcessedGongsiManager;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OpenDartReader {
+  private static final int PAGE_SIZE = 100;
+  private final OpenDartClientInterface openDartClient;
+  private final TodayProcessedGongsiManager todayProcessedGongsiManager;
+  private final RankedCompanyManager rankedCompanyManager;
+
+  public OpenDartReader(
+      OpenDartClientInterface openDartClient,
+      TodayProcessedGongsiManager todayProcessedGongsiManager,
+      RankedCompanyManager rankedCompanyManager) {
+    this.openDartClient = openDartClient;
+    this.todayProcessedGongsiManager = todayProcessedGongsiManager;
+    this.rankedCompanyManager = rankedCompanyManager;
+  }
+
+  public List<GongsiData> fetchNoticesWithPagination(LocalDate beginDate, LocalDate endDate) {
+    List<GongsiData> allNotices = new ArrayList<>();
+    int page = 1;
+
+    while (true) {
+      final int currentPage = page;
+
+      GongsiListResponse response =
+          openDartClient.getOpenDartList(
+              beginDate.toString().replace("-", ""),
+              endDate.toString().replace("-", ""),
+              currentPage,
+              PAGE_SIZE);
+
+      if (response == null
+          || response.list() == null
+          || response.list().isEmpty()
+          || currentPage > response.totalPage()) {
+        break;
+      }
+
+      for (GongsiData gongsiData : response.list()) {
+
+        if (todayProcessedGongsiManager.isProcessed(gongsiData.rceptNo())
+            || rankedCompanyManager.isNotRankedCompany(gongsiData.corpCode())) {
+          continue;
+        }
+
+        todayProcessedGongsiManager.addProcessedGongsi(gongsiData.rceptNo());
+        allNotices.add(gongsiData);
+      }
+
+      page++;
+    }
+
+    return allNotices;
+  }
+
+  public byte[] fetchGongsiDocument(String gongsiId) {
+    return openDartClient.getOpenDartDocument(gongsiId);
+  }
+}
