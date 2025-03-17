16a0c9cfd686039264f9ed4b605b40599472f6ae
goorm <goorm@goormui-MacBookPro-5.local>
2025-03-17 16:22:49 +0900

    delete: 테스트코드 삭제

--- src/main/java/org/bob/siungongsi/SiungongsiApplication.java
+++ src/main/java/org/bob/siungongsi/SiungongsiApplication.java
@@ -10,11 +10,5 @@ public class SiungongsiApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SiungongsiApplication.class, args);
-    try {
-            throw new Exception("This is a test.");
-        } catch (Exception e) {
-            Sentry.captureException(e);
-            System.out.println("Exception sent to Sentry!");
-        }
   }
 }
