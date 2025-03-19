b27b4bd278e87839fdd48535100bf2568d700a7e
Unagi-zoso <Unagi.zoso@gmail.com>
2025-03-19 14:59:53 +0900

    feat: 개발환경 Cors 관련 속성 작성
    개발환경에선 origin 구분없이 요청 받도록 변경
    대신 쿠키는 전송 받지 않도록 설정했다.
    따로 쿠키를 통해 전송하는 값이 없어 문제 없을듯하다.

---
+++ src/main/java/org/bob/siungongsi/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package org.bob.siungongsi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Profile("dev")
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
+            .allowedOrigins("*") // 모든 Origin 허용
+            .allowedMethods("*") // 모든 HTTP 메서드 허용 (GET, POST, PUT, DELETE 등)
+            .allowedHeaders("*") // 모든 요청 헤더 허용
+            .allowCredentials(false); // 쿠키 포함 허용 (true 시 allowedOrigins에 * 사용 불가)
+      }
+    };
+  }
+}
