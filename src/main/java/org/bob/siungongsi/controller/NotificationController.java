5e0eac2eec3a653c49a8f4c3d6777bced548f2e6
sein <si2238@naver.com>
2025-03-20 22:35:16 +0900

    refactor: 알림 관련 불필요한 코드 제거
    controller에서 에러 처리 삭제

--- src/main/java/org/bob/siungongsi/controller/NotificationController.java
+++ src/main/java/org/bob/siungongsi/controller/NotificationController.java
@@ -5,9 +5,7 @@ import org.bob.siungongsi.controller.dto.NotificationResponse;
 import org.bob.siungongsi.controller.spec.NotificationControllerSpec;
 import org.bob.siungongsi.dto.ApiResponseCode;
 import org.bob.siungongsi.dto.ApiResponseWrapper;
-import org.bob.siungongsi.exception.CustomException;
 import org.bob.siungongsi.service.NotificationService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +21,7 @@ public class NotificationController implements NotificationControllerSpec {
   @GetMapping("/recommended-companies")
   public ResponseEntity<ApiResponseWrapper<?>> getRecommendedCompanies(
       @RequestHeader("Authorization") String authorization) {
+
     NotificationResponse.NotificationRecommendedCompanyList companies =
         notificationService.recommendedCompanyNotification();
 
@@ -36,28 +35,10 @@ public class NotificationController implements NotificationControllerSpec {
       @RequestHeader("Authorization") String authorization,
       @RequestBody NotificationCompanyRequest request) {
 
-    try {
-      notificationService.createNotification(request);
-      return ResponseEntity.ok(
-          ApiResponseWrapper.success(ApiResponseCode.NOTIFICATION_SUBSCRIPTION_SUCCESS, null));
-    } catch (CustomException e) {
-      HttpStatus status;
+    notificationService.createNotification(request);
 
-      if (e.getErrorCode() == ApiResponseCode.NOTIFICATION_ALREADY_EXISTS) {
-        status = HttpStatus.CONFLICT; // 409
-      } else if (e.getErrorCode() == ApiResponseCode.NOTIFICATION_INVALID_COMPANY_ID) {
-        status = HttpStatus.NOT_FOUND; // 404
-      } else if (e.getErrorCode() == ApiResponseCode.NOTIFICATION_REQUIRED_STATUS) {
-        status = HttpStatus.FORBIDDEN; // 403
-      } else {
-        status = HttpStatus.BAD_REQUEST; // 기본값
-      }
-
-      return ResponseEntity.status(status).body(ApiResponseWrapper.error(e.getErrorCode()));
-    } catch (Exception e) {
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-          .body(ApiResponseWrapper.error(ApiResponseCode.NOTIFICATION_INTERNAL_SERVER_ERROR));
-    }
+    return ResponseEntity.ok(
+        ApiResponseWrapper.success(ApiResponseCode.NOTIFICATION_SUBSCRIPTION_SUCCESS, null));
   }
 
   @DeleteMapping("/{companyId}")
@@ -65,27 +46,9 @@ public class NotificationController implements NotificationControllerSpec {
       @RequestHeader("Authorization") String authorization,
       @PathVariable("companyId") Long companyId) {
 
-    try {
-      notificationService.deleteNotification(companyId);
-      return ResponseEntity.ok(
-          ApiResponseWrapper.success(ApiResponseCode.NOTIFICATION_UNSUBSCRIBE_SUCCESS, null));
-    } catch (CustomException e) {
-      HttpStatus status;
+    notificationService.deleteNotification(companyId);
 
-      if (e.getErrorCode() == ApiResponseCode.NOTIFICATION_NOT_FOUND) {
-        status = HttpStatus.CONFLICT; // 409
-      } else if (e.getErrorCode() == ApiResponseCode.NOTIFICATION_INVALID_COMPANY_ID) {
-        status = HttpStatus.NOT_FOUND; // 404
-      } else if (e.getErrorCode() == ApiResponseCode.NOTIFICATION_REQUIRED_STATUS) {
-        status = HttpStatus.FORBIDDEN; // 403
-      } else {
-        status = HttpStatus.BAD_REQUEST; // 기본값
-      }
-
-      return ResponseEntity.status(status).body(ApiResponseWrapper.error(e.getErrorCode()));
-    } catch (Exception e) {
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-          .body(ApiResponseWrapper.error(ApiResponseCode.NOTIFICATION_INTERNAL_SERVER_ERROR));
-    }
+    return ResponseEntity.ok(
+        ApiResponseWrapper.success(ApiResponseCode.NOTIFICATION_UNSUBSCRIBE_SUCCESS, null));
   }
 }
