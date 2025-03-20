2b39c34ab149385616a9bf9934df2259acd9112f
sein <si2238@naver.com>
2025-03-20 20:46:06 +0900

    refactor: RestTemplate 대신 RestClient 사용하도록 변경
    RestTemplate이 @deprecated된 이유로 RestClient로 변경

--- src/main/java/org/bob/siungongsi/service/KakaoAuthService.java
+++ src/main/java/org/bob/siungongsi/service/KakaoAuthService.java
@@ -9,6 +9,7 @@ import org.bob.siungongsi.exception.CustomException;
 import org.bob.siungongsi.repository.UserRepository;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -24,27 +25,20 @@ public class KakaoAuthService {
 
   public String validateAccessToken(String accessToken) {
     String url = "https://kapi.kakao.com/v1/user/access_token_info";
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.set("Authorization", "Bearer " + accessToken);
-
-    HttpEntity<String> entity = new HttpEntity<>(headers);
-
-    try {
-      ResponseEntity<Map> response = restTemplate.exchange(url, HttpMethod.GET, entity, Map.class);
-
-      if (response.getStatusCode() == HttpStatus.OK) {
-        Map<String, Object> body = response.getBody();
-        if (body != null) {
-          return body.get("id").toString(); // 원하는 데이터를 반환
-        }
-      }
-    } catch (Exception e) {
-      // 예외 처리 (유효하지 않은 토큰일 경우)
-      throw new CustomException(ApiResponseCode.AUTH_REQUIRED_AUTHORIZATION, "유효하지 않은 토큰입니다.");
-    }
-
-    return null; // 유효하지 않거나 예외 발생 시 null 반환
+    RestClient restClient = RestClient.create();
+    return restClient
+        .get()
+        .uri(url)
+        .header("Authorization", "Bearer " + accessToken)
+        .retrieve()
+        .onStatus(
+            HttpStatusCode::is4xxClientError,
+            ((req, res) -> {
+              throw new CustomException(ApiResponseCode.AUTH_ACCESS_TOKEN_EXPIRED, "토큰 만료입니다.");
+            }))
+        .body(Map.class)
+        .get("id")
+        .toString();
   }
 
   public Long getUserId(String socialId) {
