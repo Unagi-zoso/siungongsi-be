6d4e32118624c29dbb813a9635addc44046cb71c
Unagi-zoso <Unagi.zoso@gmail.com>
2025-03-21 00:12:55 +0900

    feat: GongsiSentStatusRepository 개발

--- src/main/java/org/bob/siungongsi/repository/GongsiSentStatusRepository.java
+++ src/main/java/org/bob/siungongsi/repository/GongsiSentStatusRepository.java
@@ -1,6 +1,15 @@
 package org.bob.siungongsi.repository;
 
-import org.bob.siungongsi.domain.GongsiSentStatusEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface GongsiSentStatusRepository extends JpaRepository<GongsiSentStatusEntity, Long> {}
+import org.bob.siungongsi.domain.GongsiSentStatusEntity;
+import org.bob.siungongsi.domain.PushStatus;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface GongsiSentStatusRepository extends JpaRepository<GongsiSentStatusEntity, Long> {
+
+  @Query("SELECT g FROM GongsiSentStatusEntity g WHERE g.status = :status")
+  List<GongsiSentStatusEntity> findByStatus(PushStatus status, Pageable pageable);
+}
