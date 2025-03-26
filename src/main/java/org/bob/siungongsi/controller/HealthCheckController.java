a0b193ec1bd4550ce645b27138820fdabcb2455c
goorm <goorm@goormui-MacBookPro-5.local>
2025-03-26 17:32:38 +0900

    chore: spotlessApply 실행

--- src/main/java/org/bob/siungongsi/controller/HealthCheckController.java
+++ src/main/java/org/bob/siungongsi/controller/HealthCheckController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/health")
-    public String healthCheck() {
-        return "ok";
-    }
+  @GetMapping("/health")
+  public String healthCheck() {
+    return "ok";
+  }
 }
