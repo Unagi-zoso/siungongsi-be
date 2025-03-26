383e2cd4580d622dedc1d46cd1d03148694b3ae1
sein <si2238@naver.com>
2025-03-26 09:23:43 +0900

    fix: 공시 상세 필터 오류
    기존 공시 상세에 토큰이 무시되는 현상 수정

--- src/main/java/org/bob/siungongsi/security/JwtAuthFilter.java
+++ src/main/java/org/bob/siungongsi/security/JwtAuthFilter.java
@@ -1,7 +1,6 @@
 package org.bob.siungongsi.security;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.bob.siungongsi.dto.ApiResponseCode;
 import org.bob.siungongsi.exception.CustomException;
@@ -15,9 +14,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class JwtAuthFilter extends OncePerRequestFilter {
   private final JwtProvider jwtProvider;
-
-  private static final List<String> REQUIRED_AUTH_APIS =
-      List.of("/v1/auth/withdraw", "/v1/notifications/**", "/v1/users/**");
 
   public JwtAuthFilter(JwtProvider jwtProvider) {
     this.jwtProvider = jwtProvider;
@@ -34,6 +30,14 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     }
 
     String authHeader = request.getHeader("Authorization");
+
+    if (request.getRequestURI().matches("/v1/gongsi/\\d+")) {
+      if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        filterChain.doFilter(request, response);
+        return;
+      }
+    }
+
     if (authHeader == null || !authHeader.startsWith("Bearer ")) {
       throw new CustomException(ApiResponseCode.AUTH_REQUIRED_AUTHORIZATION, "엑세스 토큰을 넣어주세요");
     }
@@ -51,10 +55,10 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     return uri.equals("/v1/auth/login")
         || uri.equals("/v1/auth/register")
         || uri.equals("/v1/auth/terms")
+        || uri.equals("/v1/gongsi")
         || uri.matches("/swagger-ui.*")
         || uri.matches("/admin.*")
         || uri.matches("/v1/companies.*")
-        || uri.matches("/v1/gongsi.*")
         || uri.matches("/v3/api-docs.*")
         || uri.matches("/swagger-resources.*");
   }
