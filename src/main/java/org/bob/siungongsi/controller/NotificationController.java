99c0b73ebdeb23a2a5c3ab6b958ed78ddaff2127
sein <si2238@naver.com>
2025-04-08 15:02:06 +0900

    refactor: 알림 제거 에러 처리 방식 변경

--- src/main/java/org/bob/siungongsi/controller/NotificationController.java
+++ src/main/java/org/bob/siungongsi/controller/NotificationController.java
@@ -7,7 +7,16 @@ import org.bob.siungongsi.dto.ApiResponseCode;
 import org.bob.siungongsi.dto.ApiResponseWrapper;
 import org.bob.siungongsi.service.NotificationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.constraints.Positive;
 
 @RestController
 @RequestMapping("/v1/notifications")
@@ -43,10 +52,10 @@ public class NotificationController implements NotificationControllerSpec {
         .body(ApiResponseWrapper.success(ApiResponseCode.NOTIFICATION_SUBSCRIPTION_SUCCESS, null));
   }
 
-  @DeleteMapping("/{companyId:[0-9]+}")
+  @DeleteMapping("/{companyId}")
   public ResponseEntity<ApiResponseWrapper<?>> removeNotification(
       @RequestHeader("Authorization") String authorization,
-      @PathVariable("companyId") Long companyId) {
+      @PathVariable("companyId") @Positive Long companyId) {
 
     notificationService.deleteNotification(companyId);
 
--- src/main/java/org/bob/siungongsi/exception/GlobalExceptionHandler.java
+++ src/main/java/org/bob/siungongsi/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import io.sentry.Sentry;
@@ -47,14 +48,20 @@ public class GlobalExceptionHandler {
         .body(ApiResponseWrapper.error(ApiResponseCode.API_BAD_REQUEST));
   }
 
-  @ExceptionHandler({
-    NoResourceFoundException.class,
-  })
-  public ResponseEntity<ApiResponseWrapper> handleMethodArgumentTypeMismatch(
+  @ExceptionHandler(NoResourceFoundException.class)
+  public ResponseEntity<ApiResponseWrapper> handleNoResourceFoundException(
       NoResourceFoundException ex) {
 
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+    return ResponseEntity.status(ApiResponseCode.API_BAD_REQUEST.getHttpStatus())
         .body(ApiResponseWrapper.error(ApiResponseCode.API_BAD_REQUEST));
+  }
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public ResponseEntity<ApiResponseWrapper> methodArgumentTypeMismatchException(
+      MethodArgumentTypeMismatchException ex) {
+
+    return ResponseEntity.status(ApiResponseCode.API_WRONG_TYPE_REQUEST.getHttpStatus())
+        .body(ApiResponseWrapper.error(ApiResponseCode.API_WRONG_TYPE_REQUEST));
   }
 
   // 모든 Exception을 처리하는 핸들러 (최종 예외 캐치)
