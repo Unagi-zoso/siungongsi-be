19cd9390a947e983cd08da8415a300cab8db32a7
sein <si2238@naver.com>
2025-04-08 16:42:48 +0900

    feat: 어노테이션 추가

--- src/main/java/org/bob/siungongsi/controller/NotificationController.java
+++ src/main/java/org/bob/siungongsi/controller/NotificationController.java
@@ -7,6 +7,7 @@ import org.bob.siungongsi.dto.ApiResponseCode;
 import org.bob.siungongsi.dto.ApiResponseWrapper;
 import org.bob.siungongsi.service.NotificationService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import jakarta.validation.constraints.Positive;
 
 @RestController
 @RequestMapping("/v1/notifications")
+@Validated
 public class NotificationController implements NotificationControllerSpec {
   private final NotificationService notificationService;
 
