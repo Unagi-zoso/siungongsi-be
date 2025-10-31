24eb6305c69f232fa7c430eb96a74290e095b915
Unagi_zoso <Unagi.zoso@gmail.com>
2025-11-01 03:38:20 +0900

    refactor: DevSupportController Profile 수정
    local 을 제거
    local 환경은 더이상 유효하지 않는다

--- src/main/java/org/bob/siungongsi/api/controller/DevSupportController.java
+++ src/main/java/org/bob/siungongsi/api/controller/DevSupportController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile({"dev", "local"})
+@Profile({"dev"})
 @RestController
 @RequestMapping("/support")
 public class DevSupportController implements DevSupportControllerSpec {
